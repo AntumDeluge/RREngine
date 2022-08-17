6ef8d0867e35467ee2118c990c6763da77615674
Jordan Irwin <antumdeluge@gmail.com>
2022-08-16 20:58:30 -0700

    Don't default CMake to MSYS Makefiles generator on Windows

--- PreLoad.cmake
+++
@@ -1,5 +0,0 @@
-# use MSYS generator by default on Windows systems
-if (DEFINED WIN32 AND NOT DEFINED CMAKE_GENERATOR)
-	message ("Setting generator to MSYS Makefiles")
-	set(CMAKE_GENERATOR "MSYS Makefiles" CACHE STRING "")
-endif ()
