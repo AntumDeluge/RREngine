3168012ca0b3a1570118003d83c4650c6618d90b
Jordan Irwin <antumdeluge@gmail.com>
2025-02-28 01:17:52 -0800

    FontMap class

---
+++ include/FontMap.h
@@ -0,0 +1,44 @@
+/* Copyright © 2025 Jordan Irwin <antumdeluge@gmail.com>
+ *
+ * This work is licensed under the terms of the MIT license.
+ * See: LICENSE.txt
+ */
+
+#ifndef RRE_FONT_MAP_H
+#define RRE_FONT_MAP_H
+
+#include <unordered_map>
+
+#include <SDL2/SDL_keycode.h>
+#include <SDL2/SDL_render.h>
+
+#include "Logger.h"
+
+
+/**
+ * Class to handle drawing bitmap text.
+ */
+class FontMap {
+private:
+	static Logger* logger;
+
+	SDL_Texture* texture;
+	std::unordered_map<SDL_Keycode, unsigned int> char_map;
+	/** Pixel width of each character (excluding 2 pixels of padding). */
+	unsigned int w;
+	/** Pixel height of each character (excluding 2 pixels of padding). */
+	unsigned int h;
+
+public:
+	FontMap(SDL_Texture* texture, std::unordered_map<SDL_Keycode, unsigned int> char_map,
+			unsigned int w, unsigned int h);
+
+	FontMap(std::string file_path, std::unordered_map<SDL_Keycode, unsigned int> char_map,
+			unsigned int w, unsigned int h);
+
+	SDL_Texture* getTexture() {
+		return this->texture;
+	}
+};
+
+#endif /* RRE_FONT_MAP_H */
---
+++ src/FontMap.cpp
@@ -0,0 +1,55 @@
+/* Copyright © 2025 Jordan Irwin <antumdeluge@gmail.com>
+ *
+ * This work is licensed under the terms of the MIT license.
+ * See: LICENSE.txt
+ */
+
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_surface.h>
+
+#include "Dialog.h"
+#include "FontMap.h"
+#include "fileio.h"
+#include "singletons.h"
+
+
+// initialize static members
+Logger* FontMap::logger = Logger::getLogger("FontMap");
+
+FontMap::FontMap(SDL_Texture* texture, std::unordered_map<SDL_Keycode, unsigned int> char_map,
+		unsigned int w, unsigned int h) {
+	this->texture = texture;
+	this->char_map = char_map;
+	this->w = w;
+	this->h = h;
+}
+
+FontMap::FontMap(std::string file_path, std::unordered_map<SDL_Keycode, unsigned int> char_map,
+		unsigned int w, unsigned int h) {
+	this->char_map = char_map;
+	this->w = w;
+	this->h = h;
+
+	if (!fileExists(file_path)) {
+		std::string msg = "Missing font map file: \"" + file_path + "\"";
+		FontMap::logger->error(msg);
+		Dialog::error(msg);
+	} else {
+		SDL_Surface* surface = IMG_Load(file_path.c_str());
+		if (surface == nullptr) {
+			std::string msg = "Failed to create surface from file: \"" + file_path + "\"";
+			FontMap::logger->error(msg);
+			Dialog::error(msg);
+		} else {
+			this->texture = SDL_CreateTextureFromSurface(GetViewPort()->getRenderer(), surface);
+			SDL_FreeSurface(surface);
+		}
+	}
+
+	// DEBUG:
+	//~ std::string msg = "New FontMap:\n";
+	//~ for (std::pair<int, unsigned int> p : this->char_map) {
+		//~ msg += "  " + std::to_string(p.first) + ((std::string) " => ") + std::to_string(p.second) + "\n";
+	//~ }
+	//~ Dialog::info("Debug", msg);
+}
