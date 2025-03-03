909d48d3adc8088f0d545fe01cf202bcc1535abe
Jordan Irwin <antumdeluge@gmail.com>
2025-03-03 04:24:19 -0800

    Game mode enum

---
+++ include/GameMode.h
@@ -0,0 +1,22 @@
+
+/* Copyright © 2025 Jordan Irwin <antumdeluge@gmail.com>
+ *
+ * This work is licensed under the terms of the MIT license.
+ * See: LICENSE.txt
+ */
+
+#ifndef RRE_GAME_MODE
+#define RRE_GAME_MODE
+
+
+namespace GameMode {
+	enum Mode {
+		NONE,
+		INTRO,
+		TITLE,
+		ACTIVE,
+		PAUSED
+	};
+};
+
+#endif /* RRE_GAME_MODE */
