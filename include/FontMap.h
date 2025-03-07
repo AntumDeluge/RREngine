6b0f9b384f5061eefebdd7f0d9c54324de78f146
Jordan Irwin <antumdeluge@gmail.com>
2025-03-07 13:32:48 -0800

    Derive FontMap from ImageImpl

--- include/FontMap.h
+++ include/FontMap.h
@@ -11,41 +11,28 @@
 
 #include <SDL2/SDL_render.h>
 
+#include "ImageImpl.h"
 #include "Logger.h"
 
 
 /**
  * Class to handle drawing bitmap text.
  */
-class FontMap {
+class FontMap: public ImageImpl {
 private:
 	static Logger* logger;
 
-	SDL_Texture* texture;
-	std::unordered_map<wchar_t, int> char_map;
 	/** Pixel width of each character (excluding 2 pixels of padding). */
 	uint32_t c_width;
 	/** Pixel height of each character (excluding 2 pixels of padding). */
 	uint32_t c_height;
 
-	// actual image dimensions
-	uint32_t w;
-	uint32_t h;
+	std::unordered_map<wchar_t, int> char_map;
 
 public:
 	FontMap(SDL_Texture* texture, std::unordered_map<wchar_t, int> char_map, uint32_t c_width,
 			uint32_t c_height);
 
-	~FontMap() {
-		SDL_DestroyTexture(this->texture);
-	}
-
-	SDL_Texture* getTexture() {
-		return this->texture;
-	}
-
-	uint32_t getWidth() { return this->w; }
-	uint32_t getHeight() { return this->h; }
 	uint32_t getCharWidth() { return this->c_width; }
 	uint32_t getCharHeight() { return this->c_height; }
 	int getCharIndex(wchar_t c);
--- src/FontMap.cpp
+++ src/FontMap.cpp
@@ -13,16 +13,10 @@ using namespace std;
 Logger* FontMap::logger = Logger::getLogger("FontMap");
 
 FontMap::FontMap(SDL_Texture* texture, unordered_map<wchar_t, int> char_map, uint32_t c_width,
-		uint32_t c_height) {
-	this->texture = texture;
+		uint32_t c_height): ImageImpl(texture) {
 	this->char_map = char_map;
 	this->c_width = c_width;
 	this->c_height = c_height;
-
-	int width, height;
-	SDL_QueryTexture(this->texture, nullptr, nullptr, &width, &height);
-	this->w = width;
-	this->h = height;
 }
 
 int FontMap::getCharIndex(wchar_t c) {
