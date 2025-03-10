60a8e3de4e6aa39141ffc4d5ea0d1e9ebadcf84f
Jordan Irwin <antumdeluge@gmail.com>
2025-03-10 00:02:37 -0700

    Methods for setting & getting sprite expiration

--- include/Sprite.h
+++ include/Sprite.h
@@ -7,9 +7,11 @@
 #ifndef RRE_SPRITE_H
 #define RRE_SPRITE_H
 
+#include <cstdint>
 #include <string>
 
 #include <SDL2/SDL_render.h>
+#include <SDL2/SDL_timer.h>
 
 #include "ImageImpl.h"
 
@@ -18,9 +20,20 @@
  * Sprite interface class.
  */
 class Sprite: public ImageImpl {
+private:
+	uint32_t expires = 0;
+
 public:
 	Sprite(SDL_Texture* texture): ImageImpl(texture) {}
 	Sprite(std::string id);
+
+	void setExpiration(uint32_t expires) {
+		this->expires = expires;
+	}
+
+	bool expired() {
+		return this->expires != 0 && SDL_GetTicks64() >= this->expires;
+	}
 };
 
 #endif /* RRE_SPRITE_H */
