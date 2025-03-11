0decd088d5aac747a608d72e57e7a5ba46fe92ce
Jordan Irwin <antumdeluge@gmail.com>
2025-03-10 19:57:07 -0700

    Entity class

---
+++ include/Entity.h
@@ -0,0 +1,151 @@
+/* Copyright © 2025 Jordan Irwin <antumdeluge@gmail.com>
+ *
+ * This work is licensed under the terms of the MIT license.
+ * See: LICENSE.txt
+ */
+
+#ifndef RRE_ENTITY
+#define RRE_ENTITY
+
+#include <cstdint> // uint*_t
+#include <string>
+
+#include "SDL2/SDL_rect.h"
+
+#include "Logger.h"
+#include "Sprite.h"
+
+
+/**
+ * Represents an entity object in scene.
+ */
+class Entity {
+private:
+	static Logger logger;
+
+	/** Image drawn on viewport. */
+	Sprite* sprite;
+
+	/** Entity's collision rectangle. */
+	SDL_Rect rect;
+
+public:
+	/**
+	 * Creates an entity with collision rectangle.
+	 *
+	 * @param sprite
+	 *   Entity's sprite to draw on viewport (`null` to draw nothing).
+	 * @param width
+	 *   Entity's pixel width.
+	 * @param height
+	 *   Entity's pixel height.
+	 */
+	Entity(Sprite* sprite, uint16_t width, uint16_t height);
+
+	/**
+	 * Creates an entity with collision rectangle using sprite's dimensions.
+	 *
+	 * @param sprite
+	 *   Entity's sprite to draw on viewport (`null` to draw nothing).
+	 */
+	Entity(Sprite* sprite);
+
+	/**
+	 * Creates an entity with collision rectangle.
+	 *
+	 * @param sprite_id
+	 *   String identifier to get sprite image from texture loader.
+	 * @param width
+	 *   Entity's pixel width.
+	 * @param height
+	 *   Entity's pixel height.
+	 */
+	Entity(std::string sprite_id, uint16_t width, uint16_t height);
+
+	/**
+	 * Creates an entity with collision rectangle using sprite's dimensions.
+	 *
+	 * @param sprite_id
+	 *   String identifier to get sprite image from texture loader.
+	 */
+	Entity(std::string sprite_id);
+
+	/** Default constructor. */
+	Entity(): Entity(nullptr) {}
+
+	/** Default destructor. */
+	~Entity() {
+		if (this->sprite != nullptr) {
+			delete this->sprite;
+			this->sprite = nullptr;
+		}
+	}
+
+	/**
+	 * Update entity's position.
+	 *
+	 * @param x
+	 *   New pixel position on horizontal axis.
+	 */
+	void setX(uint16_t x) { this->rect.x = x; }
+
+	/**
+	 * Update entity's position.
+	 *
+	 * @param y
+	 *   New pixel position on vertical axis.
+	 */
+	void setY(uint16_t y) { this->rect.y = y; }
+
+	/**
+	 * Change the entity's collision dimensions.
+	 *
+	 * @param width
+	 *   Entity's new pixel width.
+	 */
+	void setWidth(uint16_t width) { this->rect.w = width; }
+
+	/**
+	 * Change the entity's collision dimensions.
+	 *
+	 * @param width
+	 *   Entity's new pixel width.
+	 */
+	void setheight(uint16_t height) { this->rect.h = height; }
+
+	/**
+	 * Retrieves entity's collision dimensions.
+	 *
+	 * @return
+	 *   Collision rectangle.
+	 */
+	SDL_Rect getRect() { return this->rect; }
+
+	/**
+	 * Checks if this entity collides on a horizontal or vertical line.
+	 *
+	 * @param x
+	 *   Horizontal position of node.
+	 * @param y
+	 *   Vertial position of node.
+	 * @param len
+	 *   Pixel length of line.
+	 * @param horizontal
+	 *   Whether the line is oriented horizontally or vertically.
+	 * @return
+	 *   `true` if this entity's collision box clips vector at any point.
+	 */
+	bool collides(uint16_t x, uint16_t y, uint16_t len, bool horizontal);
+
+	/**
+	 * Checks if this entity collides with another.
+	 *
+	 * @param other
+	 *   Other entity to collision against.
+	 * @return
+	 *   `true` if entities clip at any point.
+	 */
+	bool collides(Entity* other);
+};
+
+#endif /* RRE_ENTITY */
---
+++ src/Entity.cpp
@@ -0,0 +1,79 @@
+/* Copyright © 2025 Jordan Irwin <antumdeluge@gmail.com>
+ *
+ * This work is licensed under the terms of the MIT license.
+ * See: LICENSE.txt
+ */
+
+#include "Entity.h"
+#include "Path.h"
+#include "TextureLoader.h"
+
+using namespace std;
+
+
+Logger Entity::logger = Logger::getLogger("Entity");
+
+Entity::Entity(Sprite* sprite, uint16_t width, uint16_t height) {
+	this->sprite = sprite;
+	// NOTE: are values of `SDL_Rect` implicitly set to 0 by default?
+	this->rect.x = 0;
+	this->rect.y = 0;
+	this->rect.w = width;
+	this->rect.h = height;
+}
+
+Entity::Entity(Sprite* sprite) {
+	this->sprite = sprite;
+	// NOTE: are values of `SDL_Rect` implicitly set to 0 by default?
+	this->rect.x = 0;
+	this->rect.y = 0;
+	if (this->sprite != nullptr) {
+		// default to sprite dimensions
+		this->rect.w = this->sprite->getWidth();
+		this->rect.h = this->sprite->getHeight();
+	}
+}
+
+Entity::Entity(std::string sprite_id, uint16_t width, uint16_t height):
+		Entity(new Sprite(TextureLoader::load(Path::join("sprite", sprite_id))), width, height) {}
+
+Entity::Entity(string sprite_id):
+Entity(new Sprite(TextureLoader::load(Path::join("sprite", sprite_id)))) {}
+
+
+bool Entity::collides(uint16_t x, uint16_t y, uint16_t len, bool horizontal) {
+	// this entity's right-most & bottom-most pixel collision
+	uint16_t right = this->rect.x + this->rect.w;
+	uint16_t bottom = this->rect.y + this->rect.h;
+
+	if (horizontal) {
+		uint16_t x_max = x + len;
+		if ((bottom >= y && this->rect.y <= y) && (right >= x && this->rect.x <= x_max)) {
+			return true;
+		}
+	} else {
+		uint16_t y_max = y + len;
+		if ((right >= x && this->rect.x <= x) && (bottom >= y && this->rect.y <= y_max)) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool Entity::collides(Entity* other) {
+	SDL_Rect o_rect = other->getRect();
+	// other entity's right-most & bottom-most pixel collision
+	uint16_t o_right = o_rect.x + o_rect.w;
+	//uint16_t o_top = o_rect.y;
+	uint16_t o_bottom = o_rect.y + o_rect.h;
+
+	// this entity's right-most & bottom-most pixel collision
+	uint16_t t_right = this->rect.x + this->rect.w;
+	uint16_t t_bottom = this->rect.y + this->rect.h;
+
+	// check for clipping at any point
+	return ((this->rect.x >= o_rect.x && this->rect.x <= o_right)
+			|| (t_right >= o_rect.x && t_right <= o_right))
+		&& ((this->rect.y >= o_rect.y && this->rect.y <= o_bottom)
+			|| (t_bottom >= o_rect.y && t_bottom <= o_bottom));
+}
