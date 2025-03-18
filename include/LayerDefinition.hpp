c0d61a2a5c1cbc0c1e25c282cb2f131fe36a25b1
Jordan Irwin <antumdeluge@gmail.com>
2025-03-17 20:17:50 -0700

    Layer definition type

---
+++ include/LayerDefinition.hpp
@@ -0,0 +1,13 @@
+/* Copyright © 2025 Jordan Irwin <antumdeluge@gmail.com>
+ *
+ * This work is licensed under the terms of the MIT license.
+ * See: LICENSE.txt
+ */
+
+#include <utility> // std::pair
+#include <vector>
+
+
+typedef const std::pair<uint32_t, uint8_t> GID;
+
+typedef const std::vector<GID> LayerDefinition;
