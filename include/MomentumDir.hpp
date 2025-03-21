c69cfcb68b7b3fab76815c67f5febab24d8d6ea8
AntumDeluge <antumdeluge@gmail.com>
2025-03-21 04:39:36 +0000

    Momentum deirection enumeration

---
+++ include/MomentumDir.hpp
@@ -0,0 +1,19 @@
+/* Copyright © 2025 Jordan Irwin <antumdeluge@gmail.com>
+ *
+ * This work is licensed under the terms of the MIT license.
+ * See: LICENSE.txt
+ */
+
+#ifndef RRE_MOMENTUM_DIR
+#define RRE_MOMENTUM_DIR
+
+
+namespace MomentumDir {
+	enum e {
+		NONE = 0,
+		HOR = 1,
+		VER = 2
+	}
+}
+
+#endif /* RRE_MOMENTUM_DIR */
