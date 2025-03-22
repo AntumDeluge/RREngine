d272e12f43126825006e0f72091532b9a3672faf
Jordan Irwin <antumdeluge@gmail.com>
2025-03-21 23:49:14 -0700

    Ensure unique bits in MomentumDir enum

--- include/MomentumDir.hpp
+++ include/MomentumDir.hpp
@@ -8,12 +8,12 @@
 #define RRE_MOMENTUM_DIR
 
 
-namespace MomentumDir {
-	enum e {
-		NONE = 0x00,
-		HOR = 0x01,
-		VER = 0x02
-	};
-}
+typedef enum MomentumDir {
+	NONE =  0,
+	LEFT =  1 << 0,
+	RIGHT = 1 << 1,
+	UP =    1 << 2,
+	DOWN =  1 << 3
+} MomentumDir;
 
 #endif /* RRE_MOMENTUM_DIR */
