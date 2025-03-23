a4769cd643422243951864811db1de4d465b20b6
Jordan Irwin <antumdeluge@gmail.com>
2025-03-23 03:43:11 -0700

    Hashable object class

---
+++ include/HashObject.hpp
@@ -0,0 +1,153 @@
+/* Copyright © 2025 Jordan Irwin <antumdeluge@gmail.com>
+ *
+ * This work is licensed under the terms of the MIT license.
+ * See: LICENSE.txt
+ */
+
+#ifndef RRE_HASH_OBJECT
+#define RRE_HASH_OBJECT
+
+#include <cstdint> // *int*_t
+#include <string>
+#include <unordered_map>
+
+#include "Logger.hpp"
+
+
+/**
+ * Object with hashable properties.
+ */
+class HashObject {
+private:
+	static Logger logger;
+
+	/** Hashed data. */
+	std::unordered_map<std::string, std::string> data;
+
+public:
+	/** Default constructor. */
+	HashObject() {}
+
+	/** Virtual default destructor. */
+	virtual ~HashObject() = default;
+
+	/**
+	 * Overload index operator to allow access to properties data.
+	 *
+	 * @param key
+	 *   Property key.
+	 */
+	std::string& operator[](std::string key) {
+		return data[key];
+	}
+
+	/**
+	 * Sets a property.
+	 *
+	 * @param key
+	 *   Property key.
+	 * @param value
+	 *   Property value.
+	 */
+	template <typename T>
+	void set(std::string key, T value);
+
+	/**
+	 * Removes a property.
+	 *
+	 * @param key
+	 *   Property key to remove.
+	 */
+	void unset(std::string key) {
+		data.erase(key);
+	}
+
+	/**
+	 * Checks if object has a property.
+	 *
+	 * @param key
+	 *   Propety key.
+	 * @return
+	 *   `true` if `key` index is found in properties table.
+	 */
+	bool has(std::string key) {
+		return data.find(key) != data.end();
+	}
+
+	/**
+	 * Retrieves a property value.
+	 *
+	 * @param key
+	 *   Property key.
+	 * @return
+	 *   String value or empty string if property not set.
+	 */
+	std::string get(std::string key) {
+		if (has(key)) {
+			return data[key];
+		}
+		return "";
+	}
+
+	/**
+	 * Retreivies a property integer value.
+	 *
+	 * @param key
+	 *   Property key.
+	 * @return
+	 *   Integer value or 0 if property not set.
+	 */
+	int32_t getInt(std::string key);
+
+	/**
+	 * Retreivies a property unsigned integer value.
+	 *
+	 * @param key
+	 *   Property key.
+	 * @return
+	 *   Unsigned integer value or 0 if property not set.
+	 */
+	uint32_t getUInt(std::string key);
+
+	/**
+	 * Retreivies a property long value.
+	 *
+	 * @param key
+	 *   Property key.
+	 * @return
+	 *   Long value or 0 if property not set.
+	 */
+	int64_t getLong(std::string key);
+
+	/**
+	 * Retreivies a property unsigned long value.
+	 *
+	 * @param key
+	 *   Property key.
+	 * @return
+	 *   Unsigned long value or 0 if property not set.
+	 */
+	uint64_t getULong(std::string key);
+
+	/**
+	 * Retreivies a property float value.
+	 *
+	 * @param key
+	 *   Property key.
+	 * @return
+	 *   Float value or 0 if property not set.
+	 */
+	float getFloat(std::string key);
+
+	/**
+	 * Retreivies a property double value.
+	 *
+	 * @param key
+	 *   Property key.
+	 * @return
+	 *   Double value or 0 if property not set.
+	 */
+	double getDouble(std::string key);
+};
+
+#endif /* RRE_HASH_OBJECT */
---
+++ src/HashObject.cpp
@@ -0,0 +1,95 @@
+/* Copyright © 2025 Jordan Irwin <antumdeluge@gmail.com>
+ *
+ * This work is licensed under the terms of the MIT license.
+ * See: LICENSE.txt
+ */
+
+#include <sstream>
+
+#include "HashObject.hpp"
+
+using namespace std;
+
+
+Logger HashObject::logger = Logger::getLogger("HashObject");
+
+template <typename T>
+void HashObject::set(string key, T value) {
+	stringstream ss;
+	ss << value;
+	data[key] = ss.str();
+}
+
+int32_t HashObject::getInt(string key) {
+	string s_value = get(key);
+	string err;
+	try {
+		return stoi(s_value);
+	} catch (const exception& e) {
+		err = e.what();
+	}
+	logger.error("Cannot convert key \"" + key + "\" value \"" + s_value + "\" to int: " + err);
+	return 0;
+}
+
+uint32_t HashObject::getUInt(string key) {
+	string s_value = get(key);
+	string err;
+	try {
+		return static_cast<uint32_t>(stoul(s_value));
+	} catch (const exception& e) {
+		err = e.what();
+	}
+	logger.error("Cannot convert key \"" + key + "\" value \"" + s_value + "\" to unsigned int: "
+			+ err);
+	return 0;
+}
+
+int64_t HashObject::getLong(string key) {
+	string s_value = get(key);
+	string err;
+	try {
+		return stoll(s_value);
+	} catch (const exception& e) {
+		err = e.what();
+	}
+	logger.error("Cannot convert key \"" + key + "\" value \"" + s_value + "\" to long: " + err);
+	return 0;
+}
+
+uint64_t HashObject::getULong(string key) {
+	string s_value = get(key);
+	string err;
+	try {
+		return stoull(s_value);
+	} catch (const exception& e) {
+		err = e.what();
+	}
+	logger.error("Cannot convert key \"" + key + "\" value \"" + s_value + "\" to unsigned long: "
+			+ err);
+	return 0;
+}
+
+float HashObject::getFloat(string key) {
+	string s_value = get(key);
+	string err;
+	try {
+		return stof(s_value);
+	} catch (const exception& e) {
+		err = e.what();
+	}
+	logger.error("Cannot convert key \"" + key + "\" value \"" + s_value + "\" to float: " + err);
+	return 0;
+}
+
+double HashObject::getDouble(string key) {
+	string s_value = get(key);
+	string err;
+	try {
+		return stod(s_value);
+	} catch (const exception& e) {
+		err = e.what();
+	}
+	logger.error("Cannot convert key \"" + key + "\" value \"" + s_value + "\" to double: " + err);
+	return 0;
+}
