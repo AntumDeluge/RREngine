5e63b1c2ebe229f6549abceac80e76bb43c4b845
Jordan Irwin <antumdeluge@gmail.com>
2025-04-27 21:03:51 -0700

    Skeleton class for parallax scrolling images

---
+++ include/ParallaxImage.hpp
@@ -0,0 +1,16 @@
+/* Copyright © 2025 Jordan Irwin <antumdeluge@gmail.com>
+ *
+ * This work is licensed under the terms of the MIT license.
+ * See: LICENSE.txt
+ */
+
+#ifndef RRE_PARALLAX_IMAGE
+#define RRE_PARALLAX_IMAGE
+
+#include "Image.hpp"
+
+
+class ParallaxImage: public Image {
+};
+
+#endif /* RRE_PARALLAX_IMAGE */
---
+++ src/ParallaxImage.cpp
@@ -0,0 +1,7 @@
+/* Copyright © 2025 Jordan Irwin <antumdeluge@gmail.com>
+ *
+ * This work is licensed under the terms of the MIT license.
+ * See: LICENSE.txt
+ */
+
+#include "ParallaxImage.hpp"
