de8d5afda3e17569643828e25f333ec60309bd8f
Jordan Irwin <antumdeluge@gmail.com>
2025-04-27 17:58:23 -0700

    Link to SDL2_ttf

--- cmake/depends.cmake
+++ cmake/depends.cmake
@@ -10,11 +10,15 @@ endif()
 pkg_search_module(SDL2 sdl2>=2.0.18 ${MODULE_PARAMS})
 pkg_search_module(SDL2MIXER SDL2_mixer>=2.0.0 ${MODULE_PARAMS})
 pkg_search_module(SDL2IMAGE SDL2_image>=2.0.0 ${MODULE_PARAMS})
+pkg_search_module(SDL2TTF SDL2_ttf>=2.0.0 ${MODULE_PARAMS})
 if(NOT SDL2MIXER_VERSION)
 	message(FATAL_ERROR "Required library SDL2_mixer or compatible version (>=2.0.0) not found")
 endif()
 if(NOT SDL2IMAGE_VERSION)
 	message(FATAL_ERROR "Required library SDL2_image or compatible version (>=2.0.0) not found")
+endif()
+if(NOT SDL2TTF_VERSION)
+	message(FATAL_ERROR "Required library SDL2_ttf or compatible version (>=2.0.0) not found")
 endif()
 
 # pugixml
@@ -57,6 +61,7 @@ include_directories(
 	"${SDL2_INCLUDE_DIRS}"
 	"${SDL2MIXER_INCLUDE_DIRS}"
 	"${SDL2IMAGE_INCLUDE_DIRS}"
+	"${SDL2TTF_INCLUDE_DIRS}"
 	# bundled libraries
 	"${PROJECT_SOURCE_DIR}/lib/cxxopts"
 	#"${PROJECT_SOURCE_DIR}/lib/lua/src"
@@ -73,6 +78,7 @@ if(NOT STATIC)
 		${SDL2_LIBRARIES}
 		${SDL2MIXER_LIBRARIES}
 		${SDL2IMAGE_LIBRARIES}
+		${SDL2TTF_LIBRARIES}
 		${PUGIXML_LIBRARIES}
 		${TMXLITE_LIBRARIES}
 	)
@@ -81,6 +87,7 @@ else()
 		${SDL2_STATIC_LIBRARIES}
 		${SDL2MIXER_STATIC_LIBRARIES}
 		${SDL2IMAGE_STATIC_LIBRARIES}
+		${SDL2TTF_STATIC_LIBRARIES}
 		${PUGIXML_STATIC_LIBRARIES}
 		${TMXLITE_STATIC_LIBRARIES}
 	)
