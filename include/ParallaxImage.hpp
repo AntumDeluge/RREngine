d9aaad857739519d4a7db0f93ee50fbda43e0f8b
Jordan Irwin <antumdeluge@gmail.com>
2025-04-28 00:51:44 -0700

    Create constructor & rendering methods for parallax image class

--- include/ParallaxImage.hpp
+++ include/ParallaxImage.hpp
@@ -7,10 +7,64 @@
 #ifndef RRE_PARALLAX_IMAGE
 #define RRE_PARALLAX_IMAGE
 
+#include <cstdint> // *int*_t
+
+#include <SDL2/SDL_render.h>
+
 #include "Image.hpp"
+#include "Logger.hpp"
+#include "Renderer.hpp"
 
 
+/** Scrolling axis enumeration. */
+enum ORIENT {
+	HORIZONTAL,
+	VERTICAL,
+	ALL
+};
+
+/**
+ * Represents an image that scrolls at specified rate.
+ */
 class ParallaxImage: public Image {
+private:
+	static Logger logger;
+
+	/** Scrolling direction. */
+	ORIENT orientation;
+
+public:
+	/**
+	 * Creates a new parallax image.
+	 *
+	 * @param texture
+	 *   Texture to draw with renderer.
+	 */
+	ParallaxImage(SDL_Texture* texture): Image(texture) {
+		orientation = HORIZONTAL;
+	}
+
+	/**
+	 * Sets scrolling axis.
+	 *
+	 * @param orient
+	 *   Horizontal and/or vertical axis.
+	 */
+	void setOrientation(ORIENT orient) {
+		orientation = orient;
+	}
+
+	/**
+	 * Draws image on renderer.
+	 *
+	 * @param ctx
+	 *   Drawing renderer.
+	 * @param x
+	 *   Player horizontal position.
+	 * @param y
+	 *   Player vertical position.
+	 */
+	void render(Renderer* ctx, uint32_t x, uint32_t y);
 };
 
 #endif /* RRE_PARALLAX_IMAGE */
--- src/ParallaxImage.cpp
+++ src/ParallaxImage.cpp
@@ -5,3 +5,36 @@
  */
 
 #include "ParallaxImage.hpp"
+
+
+Logger ParallaxImage::logger = Logger::getLogger("ParallaxImage");
+
+void ParallaxImage::render(Renderer* ctx, uint32_t x, uint32_t y) {
+	if (orientation == HORIZONTAL) {
+		uint32_t width_max = ctx->getInternalWidth();
+		uint32_t dx = 0;
+		while (dx < width_max) {
+			ctx->drawImage(this, dx, 0);
+			dx += this->width;
+		}
+	} else if (orientation == VERTICAL) {
+		uint32_t height_max = ctx->getInternalHeight();
+		uint32_t dy = 0;
+		while (dy < height_max) {
+			ctx->drawImage(this, 0, dy);
+			dy += this->height;
+		}
+	} else {
+		uint32_t width_max = ctx->getInternalWidth();
+		uint32_t height_max = ctx->getInternalHeight();
+		uint32_t dx = 0;
+		while (dx < width_max) {
+			uint32_t dy = 0;
+			while (dy < height_max) {
+				ctx->drawImage(this, dx, dy);
+				dy += this->height;
+			}
+			dx += this->width;
+		}
+	}
+}
